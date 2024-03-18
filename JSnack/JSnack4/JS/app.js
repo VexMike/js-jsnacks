492f8f7fda8e131933890d5e174faf51fbaf769b
Urbani Michele <michele.urbani094@gmail.com>
2024-03-18 18:24:47 +0100

    ADD: JSnack4 da finire

--- JSnack/JSnack4/JS/app.js
+++ JSnack/JSnack4/JS/app.js
@@ -1,2 +1,26 @@
 console.log('JS Snacks 4')
 
+// Inseriamo in un Array l'elenco degli invitati alla festa
+// - Chiedere all'utente il nome
+// - SE c'è può partecipare
+// - ALTRIMENTI non può partecipare
+
+// Lista invitati
+const nomi = ['Mario', 'Luigi', 'Daisy', 'Boo', 'Bowser'];
+
+//Chiediamo all'utente il nome
+const nomeInvitato = prompt('Mi dica il suo nome');
+console.log(nomeInvitato);
+
+// Verifichiamo se l'utente è nella lista
+
+for (i = 0; i < nomi; i++) {
+    if (nomi[i] === nomeInvitato) {
+        console.log('Il signor' + nomeInvitato + 'può entrare');
+        break;
+    }
+    // Se non è nella lista
+    else {
+        alert('Non puoi entrare')
+    }
+}
