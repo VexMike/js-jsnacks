a788c4afd4f0264d8e3638f369548e416dd17cce
Urbani Michele <michele.urbani094@gmail.com>
2024-03-18 20:58:31 +0100

    ADD: Fix JSnack 2 length e console.log

--- JSnack/JSnack2/JS/app.js
+++ JSnack/JSnack2/JS/app.js
@@ -16,17 +16,19 @@ const par2 = prompt("Inserisci la parola"); // String
 if (isNaN(par1) && isNaN(par2)) {
   // Inserisco la prima parola e SE
   const par1Lungh = par1.length;
-  const par2Lungh = par2.lenght;
+  const par2Lungh = par2.length;
 
   if (par1Lungh < par2Lungh) {
-    console.log(par1Lungh, par2Lungh);
+    console.log(par1, par2);
   }
 
   // ALTRIMENTI SE inserisco la seconda parola
   else if (par2Lungh < par1Lungh) {
-    console.log(par2Lungh, par1Lungh);
+    console.log(par2, par1);
   }
-
+  else {
+    console.log('Sono uguali')
+  }
   // ALTRIMENTI i dati inseriti non sono validi
 } else {
   alert("Dati inseriti non validi");
