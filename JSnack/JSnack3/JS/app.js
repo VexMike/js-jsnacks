86bd7c47aef044268908c90cd88401f63f423ba4
Urbani Michele <michele.urbani094@gmail.com>
2024-03-18 17:44:17 +0100

    ADD: JSnack3 da finire

--- JSnack/JSnack3/JS/app.js
+++ JSnack/JSnack3/JS/app.js
@@ -1,3 +1,16 @@
 console.log('JS Snacks 3')
 
+// Chiedere all'utente di inserire 10 numeri.
+// - Il programma stampa la somma di tutti i numeri.
 
+// Inseriamo la variabile
+let numero1 = 0;
+// console.log(somma)
+
+// Chiediamo 10 volte un numero all'utente
+for (let i = 0; i < 10; i++) {
+    numero2 = parseInt(prompt('Inserisci un numero'));
+    somma = numero1 +numero2
+}
+
+console.log('Somma totale dei numeri inseriti: ')
